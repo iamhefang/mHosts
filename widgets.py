24f54ddec75fe76a306320e0242c16f4e043f252
何方 <he@hefang.link>
2019-07-06 12:40:22 +0800

    添加vscode调试配置文件

--- widgets.py
+++ widgets.py
@@ -28,32 +28,39 @@ class MainFrame(Frame):
         self.SetSizeHints(Size(500 * dpi[0], 350 * dpi[1]))
         self.menuBar = MenuBar(0)
         self.menuFile = Menu()
-        self.menuItemNew = MenuItem(self.menuFile, ID_ANY, u"新建(&N)", EmptyString, ITEM_NORMAL)
-        self.menuItemImport = MenuItem(self.menuFile, ID_ANY, u"导入(&I)", EmptyString, ITEM_NORMAL)
+        self.menuItemNew = MenuItem(
+            self.menuFile, ID_ANY, u"新建(&N)", EmptyString, ITEM_NORMAL)
+        self.menuItemImport = MenuItem(
+            self.menuFile, ID_ANY, u"导入(&I)", EmptyString, ITEM_NORMAL)
         self.menuItemImport.Enable(False)
         self.menuFile.Append(self.menuItemNew)
         self.menuFile.Append(self.menuItemImport)
 
         self.menuFile.AppendSeparator()
 
-        self.menuItemExit = MenuItem(self.menuFile, ID_ANY, u"退出(&Q)", EmptyString, ITEM_NORMAL)
+        self.menuItemExit = MenuItem(
+            self.menuFile, ID_ANY, u"退出(&Q)", EmptyString, ITEM_NORMAL)
         self.menuFile.Append(self.menuItemExit)
 
         self.menuBar.Append(self.menuFile, u"文件(&F)")
 
         self.menuHelp = Menu()
 
-        self.menuItemSettings = MenuItem(self.menuHelp, ID_ANY, u"首选项(&P)", EmptyString, ITEM_NORMAL)
+        self.menuItemSettings = MenuItem(
+            self.menuHelp, ID_ANY, u"首选项(&P)", EmptyString, ITEM_NORMAL)
         self.menuItemSettings.Enable(False)
         self.menuHelp.Append(self.menuItemSettings)
 
-        self.menuItemHelpDoc = MenuItem(self.menuHelp, ID_ANY, u"帮助文档(&D)", EmptyString, ITEM_NORMAL)
+        self.menuItemHelpDoc = MenuItem(
+            self.menuHelp, ID_ANY, u"帮助文档(&D)", EmptyString, ITEM_NORMAL)
         self.menuHelp.Append(self.menuItemHelpDoc)
 
-        self.menuItemCheckUpdate = MenuItem(self.menuHelp, ID_ANY, u"检查更新(&U)", EmptyString, ITEM_NORMAL)
+        self.menuItemCheckUpdate = MenuItem(
+            self.menuHelp, ID_ANY, u"检查更新(&U)", EmptyString, ITEM_NORMAL)
         self.menuHelp.Append(self.menuItemCheckUpdate)
 
-        self.menuItemAbout = MenuItem(self.menuHelp, ID_ANY, u"关于(&A)", EmptyString, ITEM_NORMAL)
+        self.menuItemAbout = MenuItem(
+            self.menuHelp, ID_ANY, u"关于(&A)", EmptyString, ITEM_NORMAL)
         self.menuHelp.Append(self.menuItemAbout)
 
         self.menuBar.Append(self.menuHelp, u"帮助(&H)")
@@ -61,7 +68,8 @@ class MainFrame(Frame):
         self.SetMenuBar(self.menuBar)
 
         self.statusBar = StatusBar(self, ID_ANY, STB_SIZEGRIP)
-        statusBarFont = Font(10, FONTFAMILY_DEFAULT, FONTSTYLE_NORMAL, FONTWEIGHT_NORMAL, False, EmptyString)
+        statusBarFont = Font(
+            10, FONTFAMILY_DEFAULT, FONTSTYLE_NORMAL, FONTWEIGHT_NORMAL, False, EmptyString)
         self.statusBar.SetFont(statusBarFont)
         self.SetStatusBar(self.statusBar)
 
@@ -69,9 +77,9 @@ class MainFrame(Frame):
 
         self.images = {}
 
-        self.imageList = ImageList()
-
-        for name, bitmap in GetIcons().items():
+        self.imageList = ImageList(width=12, height=12)
+        iconList = GetIcons().items()
+        for name, bitmap in iconList:
             self.images[name] = self.imageList.Add(bitmap)
 
         self.hostsTree = TreeCtrl(
@@ -99,10 +107,13 @@ class MainFrame(Frame):
         self.Bind(EVT_MENU, self.OnMenuClicked, id=self.menuItemNew.GetId())
         self.Bind(EVT_MENU, self.OnMenuClicked, id=self.menuItemExit.GetId())
         self.Bind(EVT_MENU, self.OnMenuClicked, id=self.menuItemAbout.GetId())
-        self.Bind(EVT_MENU, self.OnMenuClicked, id=self.menuItemHelpDoc.GetId())
-        self.Bind(EVT_MENU, self.OnMenuClicked, id=self.menuItemSettings.GetId())
+        self.Bind(EVT_MENU, self.OnMenuClicked,
+                  id=self.menuItemHelpDoc.GetId())
+        self.Bind(EVT_MENU, self.OnMenuClicked,
+                  id=self.menuItemSettings.GetId())
         self.Bind(EVT_MENU, self.OnMenuClicked, id=self.menuItemImport.GetId())
-        self.Bind(EVT_MENU, self.OnMenuClicked, id=self.menuItemCheckUpdate.GetId())
+        self.Bind(EVT_MENU, self.OnMenuClicked,
+                  id=self.menuItemCheckUpdate.GetId())
 
     def __del__(self):
         pass
@@ -117,7 +128,7 @@ class MainFrame(Frame):
 
 
 ###########################################################################
-## Class AboutDialog
+# Class AboutDialog
 ###########################################################################
 
 class AboutDialog(Dialog):
@@ -165,51 +176,62 @@ class EditDialog(Dialog):
         self.__window = parent
         self.SetSizeHints(DefaultSize, DefaultSize)
 
-        font = Font(10, FONTFAMILY_DEFAULT, FONTSTYLE_NORMAL, FONTWEIGHT_NORMAL, False, EmptyString)
+        font = Font(10, FONTFAMILY_DEFAULT, FONTSTYLE_NORMAL,
+                    FONTWEIGHT_NORMAL, False, EmptyString)
         inputSize = Size(260 * dpi[0], -1)
         self.SetFont(font)
         fgSizer3 = FlexGridSizer(0, 2, 0, 0)
         fgSizer3.SetFlexibleDirection(BOTH)
         fgSizer3.SetNonFlexibleGrowMode(FLEX_GROWMODE_SPECIFIED)
 
-        self.localRadio = RadioButton(self, ID_ANY, u"本地Hosts", DefaultPosition, DefaultSize, 0)
+        self.localRadio = RadioButton(
+            self, ID_ANY, u"本地Hosts", DefaultPosition, DefaultSize, 0)
         self.localRadio.SetFont(font)
         fgSizer3.Add(self.localRadio, 0, ALL, 5)
         self.localRadio.Bind(EVT_RADIOBUTTON, self.OnRadioChange)
 
-        self.onlineRadio = RadioButton(self, ID_ANY, u"在线Hosts", DefaultPosition, DefaultSize, 0)
+        self.onlineRadio = RadioButton(
+            self, ID_ANY, u"在线Hosts", DefaultPosition, DefaultSize, 0)
         fgSizer3.Add(self.onlineRadio, 0, ALL, 5)
         self.onlineRadio.Bind(EVT_RADIOBUTTON, self.OnRadioChange)
 
-        self.m_staticText4 = StaticText(self, ID_ANY, u"名称", DefaultPosition, DefaultSize, 0)
+        self.m_staticText4 = StaticText(
+            self, ID_ANY, u"名称", DefaultPosition, DefaultSize, 0)
         self.m_staticText4.Wrap(-1)
 
         fgSizer3.Add(self.m_staticText4, 0, ALL, 5)
 
-        self.nameInput = TextCtrl(self, ID_ANY, EmptyString, DefaultPosition, inputSize, 0)
+        self.nameInput = TextCtrl(
+            self, ID_ANY, EmptyString, DefaultPosition, inputSize, 0)
         fgSizer3.Add(self.nameInput, 0, ALL, 5)
 
-        self.m_staticText5 = StaticText(self, ID_ANY, u"地址", DefaultPosition, DefaultSize, 0)
+        self.m_staticText5 = StaticText(
+            self, ID_ANY, u"地址", DefaultPosition, DefaultSize, 0)
         self.m_staticText5.Wrap(-1)
 
         fgSizer3.Add(self.m_staticText5, 0, ALL, 5)
 
-        self.urlInput = TextCtrl(self, ID_ANY, u"http://", DefaultPosition, inputSize, 0)
+        self.urlInput = TextCtrl(
+            self, ID_ANY, u"http://", DefaultPosition, inputSize, 0)
         fgSizer3.Add(self.urlInput, 0, ALL, 5)
 
-        self.m_staticText3 = StaticText(self, ID_ANY, u"图标", DefaultPosition, DefaultSize, 0)
+        self.m_staticText3 = StaticText(
+            self, ID_ANY, u"图标", DefaultPosition, DefaultSize, 0)
         self.m_staticText3.Wrap(-1)
 
         fgSizer3.Add(self.m_staticText3, 0, ALL, 5)
 
-        self.iconComboBox = ComboBox(self, ID_ANY, u"请选择图标", DefaultPosition, inputSize, list(GetIcons().keys()), 0)
+        self.iconComboBox = ComboBox(
+            self, ID_ANY, u"请选择图标", DefaultPosition, inputSize, list(GetIcons().keys()), 0)
         self.iconComboBox.SetFont(font)
         fgSizer3.Add(self.iconComboBox, 0, ALL, 5)
 
-        self.cancelButton = Button(self, ID_ANY, u"取消", DefaultPosition, DefaultSize, 0)
+        self.cancelButton = Button(
+            self, ID_ANY, u"取消", DefaultPosition, DefaultSize, 0)
         fgSizer3.Add(self.cancelButton, 0, ALL, 5)
 
-        self.saveButton = Button(self, ID_ANY, u"保存", DefaultPosition, DefaultSize, 0)
+        self.saveButton = Button(
+            self, ID_ANY, u"保存", DefaultPosition, DefaultSize, 0)
         fgSizer3.Add(self.saveButton, 0, ALL, 5)
 
         self.SetSizer(fgSizer3)
@@ -274,7 +296,8 @@ class EditDialog(Dialog):
                     name,
                     url=url,
                     lastUpdateTime=Now(),
-                    content="# Created by mHosts v%s, %s\n" % (Settings.version(), Now())
+                    content="# Created by mHosts v%s, %s\n" % (
+                        Settings.version(), Now())
                 ))
             Settings.Save()
             self.__window.InitHostsTree(select=hostsId)
